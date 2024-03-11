7751bfc4d5e7c98e8305c6f842de6c67644db677
fabio silva <109297324+FabioSilva11@users.noreply.github.com>
2024-03-11 14:58:09 -0400

    Create Bot pix cópia e cola.py

---
+++ Bot pix cópia e cola.py
@@ -0,0 +1,34 @@
+import datetime
+import mercadopago
+import telebot
+
+sdk = mercadopago.SDK(TOKEN_MERCADOPAGO)
+bot = telebot.TeleBot(TOKEN_BOT)
+
+def create_payment(value):
+    expire = datetime.datetime.now() + datetime.timedelta(days=1)
+    expire = expire.strftime("%Y-%m-%dT%-H:%M:%S.000-03:00")
+
+    payment_data = {
+        "transaction_amount": int(value),
+        "payment_method_id": 'pix',
+        "installments": 1,
+        "description": 'Descrição',
+        "date_of_expiration": f"{expire}",
+        "payer": {
+            "email": 'email@dominio.xpto'
+        }
+    }
+    result = sdk.payment().create(payment_data)
+    return result
+
+@bot.message_handler(commands=['pagar'])
+def cmd_pagar(message):
+    payment = create_payment(10)
+    pix_copia_cola = payment['response']['point_of_interaction']['transaction_data']['qr_code']
+   
+
+ bot.send_message(message.from_user.id, f'<code>{pix_copia_cola}</code>', parse_mode='HTML')
+
+if __name__ == "__main__":
+    bot.infinity_polling()
